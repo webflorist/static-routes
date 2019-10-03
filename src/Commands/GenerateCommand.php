a049e7f2960c24373d10d75e2fc8f0cc1ba009a4
Gerald Buttinger <gerald@code.florist>
2019-10-03 21:47:41 +0200

    crawl using app()->handle

--- src/Commands/GenerateCommand.php
+++ src/Commands/GenerateCommand.php
@@ -3,8 +3,11 @@
 namespace Webflorist\StaticRoutes\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Route;
 use Illuminate\Routing\Router;
+use Webflorist\FormFactory\FormFactory;
 use Webflorist\StaticRoutes\Crawlers\HttpRequestCrawler;
 use Webflorist\StaticRoutes\Exceptions\RouteGenerationException;
 
@@ -24,15 +27,28 @@ class GenerateCommand extends Command
      * @var string
      */
     protected $description = 'Generates static routes.';
+    
+    /**
+     * @var Router
+     */
+    private $router;
+    
+    /**
+     * @var Request
+     */
+    private $request;
 
     /**
      * Create a new command instance.
      *
-     * @param HttpRequestCrawler $crawler
+     * @param Router $router
+     * @param Request $request
      */
-    public function __construct()
+    public function __construct(Router $router, Request $request)
     {
         parent::__construct();
+        $this->router = $router;
+        $this->request = $request;
     }
 
     /**
@@ -42,7 +58,7 @@ class GenerateCommand extends Command
      * @return mixed
      * @throws RouteGenerationException
      */
-    public function handle(Router $router)
+    public function handle(Router $router, Request $request)
     {
         // Set environment to production.
         config()->set('app.env', 'production');
@@ -50,8 +66,6 @@ class GenerateCommand extends Command
 
         // Set debug to false
         config()->set('app.debug', false);
-
-        $crawler = new HttpRequestCrawler(app());
 
         // Set a pseudo-config key to let application know,
         // a static generation is happening.
@@ -61,7 +75,8 @@ class GenerateCommand extends Command
 
         self::rrmdir($outputBasePath);
 
-        foreach ($router->getRoutes()->getRoutesByMethod()['GET'] as $route) {
+        foreach ($this->router->getRoutes()->getRoutesByMethod()['GET'] as $route) {
+            /** @var Route $route */
 
             $uri = $route->uri();
 
@@ -70,17 +85,7 @@ class GenerateCommand extends Command
                 continue;
             }
 
-            /** @var Route $route */
-            $outputFile = "$outputBasePath/$uri/index.html";
-            $outputPath = substr($outputFile, 0, strrpos($outputFile, '/'));
-
-            if (!file_exists($outputPath)) {
-                mkdir($outputPath, 0777, true);
-            }
-
-            $response = $crawler->get(
-                $uri
-            );
+            $response = $this->getResponse($uri);
 
             if (!is_null($response->exception)) {
                 $exceptionInfo = get_class($response->exception).':'.$response->exception->getMessage();
@@ -88,13 +93,15 @@ class GenerateCommand extends Command
             }
 
             if ($response->isRedirection()) {
-	        // TODO: Handle redirections.
-                $redirectTarget = $response->baseResponse->headers->get('Location');
+	            // TODO: Handle redirections.
                 $this->line("$uri: excluded due to redirection.");
                 continue;
             }
 
-            file_put_contents($outputFile, $response->content());
+            file_put_contents(
+                $this->getOutputFile($outputBasePath, $uri),
+                $response->content()
+            );
 
             $this->info("$uri: generated successfully.");
 
@@ -141,5 +148,40 @@ class GenerateCommand extends Command
             }
         }
         return false;
+    }
+
+    /**
+     * @param string $outputBasePath
+     * @param string $uri
+     * @return string
+     */
+    protected function getOutputFile(string $outputBasePath, string $uri): string
+    {
+        $outputFile = "$outputBasePath/$uri/index.html";
+        $outputPath = substr($outputFile, 0, strrpos($outputFile, '/'));
+
+        if (!file_exists($outputPath)) {
+            mkdir($outputPath, 0777, true);
+        }
+        return $outputFile;
+    }
+
+    /**
+     * @param string $uri
+     * @return Response
+     */
+    protected function getResponse(string $uri)
+    {
+
+        // Reload formfactory singleton.
+        app()->forgetInstance('Webflorist\FormFactory\FormFactory');
+
+        $request = Request::create($uri);
+        $request->headers->set('HOST',
+            $this->request->headers->get('HOST')
+        );
+        /** @var Response $response */
+        $response = $response = app()->handle($request);
+        return $response;
     }
 }
